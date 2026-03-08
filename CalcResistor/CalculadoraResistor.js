fe4e91330fd4fbfa33b01ae4dd1c25e2e7afe50c
Yago Scoz <yagoscoz10@gmail.com>
2026-03-07 21:15:00 -0300

    Atualizacao de casa

--- CalcResistor/CalculadoraResistor.js
+++ CalcResistor/CalculadoraResistor.js
@@ -1,4 +1,3 @@
 /*Imput : se é 4 ou 5 faixas
 input : faixa 1 , 2 , 3 , 4 , 5
 */
-
--- CalcUnidades/CalculadoraCompletaMulti&Sub.js
+++ CalcUnidades/CalculadoraCompletaMulti&Sub.js
@@ -1,10 +1,6 @@
 const prefixo = "G" = 10**9 ; "M" = 10**6 ; "k" ; ""
 
 
-
-
-
-
 let unidade = prompt(`Digite qual unidade voce deseja utilizar:
 Corrente elétrica → A (Ampère)
 Tensão elétrica → V (Volt)
@@ -16,4 +12,5 @@ let escolhaConversao = parseInt(prompt(`Digite qual conversao você deseja fazer
 let quantidade = parseFloat(prompt(`Digite o valor da grandeza na unidade base para transformalo em mini :`))
 let conversao = quantidade * 1000
 alert(quantidade + ` ` + unidade + ` representam : ` + conversao + ` m` + unidade )
-console.log(quantidade + ` ` + unidade + ` representam : ` + conversao + ` m` + unidade )
+console.log(quantidade + ` ` + unidade + ` representam : ` + conversao + ` m` + unidade );
+
