70359871131000efc7aa09797548fd7fb2f1ad3e
yago.scz <yagoscoz10@gmail.com>
2026-03-09 20:28:47 -0300

    Atualizacao da calculadora de resistor.

--- CalcResistor/CalculadoraResistor.js
+++ CalcResistor/CalculadoraResistor.js
@@ -1,3 +1,15 @@
 /*Imput : se é 4 ou 5 faixas
 input : faixa 1 , 2 , 3 , 4 , 5
 */
+const faixasNormal = {};
+const tolerancia = {};
+const multiplicador = {};
+
+let qtdFaixas = parseInt(
+    prompt(`Digite a quantidade de faixas que o resistor possui :
+(4) para resistores com 4 faixas
+(5) para resistores com 5 faixas`));
+switch(qtdFaixas){
+    case 4 :
+        let faixa1 = prompt("Digite a cor da 1 faixa : ")
+}
