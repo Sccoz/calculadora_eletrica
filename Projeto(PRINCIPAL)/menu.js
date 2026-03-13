acd04f77fdff5293ab0c9636725b8319bcdf8266
yago.scz <yagoscoz10@gmail.com>
2026-03-13 15:30:00 -0300

    Criacao do Projeto Principal para juntar todas as calculadoras

---
+++ Projeto(PRINCIPAL)/menu.js
@@ -0,0 +1,4 @@
+let opcao = parseInt(peompt(`Qual calculadora voce deseja usar :
+(1) -> Calculadora do consumo de um eletrodomestico por mes
+(2) -> Conversor de unidades
+(3) -> Calculadora de Ohm de um resistor`))
